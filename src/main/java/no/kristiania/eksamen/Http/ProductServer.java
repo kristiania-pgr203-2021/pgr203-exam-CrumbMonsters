861e9ad707040d17977ba90e4b5725a755a2bdfc
lbjornbak <69962560+lbjornbak@users.noreply.github.com>
2021-11-02 10:52:03 +0100

    Remove encryption as it turns out to not have any use due to everything being local.

--- src/main/java/no/kristiania/eksamen/Http/ProductServer.java
+++ src/main/java/no/kristiania/eksamen/Http/ProductServer.java
@@ -5,18 +5,14 @@ import no.kristiania.eksamen.question.QuestionDao;
 import org.flywaydb.core.Flyway;
 import org.postgresql.ds.PGSimpleDataSource;
 
-import javax.crypto.spec.SecretKeySpec;
 import javax.sql.DataSource;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.util.Properties;
-
-import static no.kristiania.eksamen.Http.Encryption.*;
 
 public class ProductServer {
 
-    public static void main(String[] args) throws IOException, GeneralSecurityException {
+    public static void main(String[] args) throws IOException {
         DataSource dataSource = createDataSource();
         QuestionDao questionDao = new QuestionDao (dataSource);
         AnswerDao answerDao = new AnswerDao (dataSource);
@@ -24,36 +20,16 @@ public class ProductServer {
         httpServer.addController("/api/questions", new ListQuestionsController(questionDao));
     }
 
-    private static DataSource createDataSource() throws IOException, GeneralSecurityException {
+    private static DataSource createDataSource() throws IOException {
         FileInputStream fis = new FileInputStream("src/main/resources/properties/config.properties");
         Properties properties = new Properties();
         properties.load(fis);
 
-        byte[] salt = "12345678".getBytes();
-        int iterationCount = 40000;
-        int keyLength = 128;
-        String password = properties.getProperty("password");
 
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
         dataSource.setUrl(properties.getProperty("URL"));
         dataSource.setUser(properties.getProperty("username"));
-
-        if (password == null) {
-            throw new IllegalArgumentException("Run with -Dpassword=<password>");
-        }
-
-        SecretKeySpec key = createSecretKey(password.toCharArray(), salt, iterationCount, keyLength);
-
-
-        //Remember to remove
-        System.out.println("Original password: " + password);
-        String encryptedPassword = encrypt(password, key);
-        System.out.println("Encrypted password: " + encryptedPassword);
-        String decryptedPassword = decrypt(encryptedPassword, key);
-        System.out.println("Decrypted password: " + decryptedPassword);
-
-        dataSource.setPassword(decryptedPassword);
-
+        dataSource.setPassword(properties.getProperty("password"));
 
         Flyway.configure().dataSource(dataSource).load();
 
