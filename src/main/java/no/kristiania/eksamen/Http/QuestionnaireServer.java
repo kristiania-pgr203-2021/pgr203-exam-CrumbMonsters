e1e2551ad9f8656320d3040148755fbe8db7d375
lbjornbak <69962560+lbjornbak@users.noreply.github.com>
2021-11-02 11:00:02 +0100

    refactor name productServer to QuestionnaireServer in main and pom.xml

--- src/main/java/no/kristiania/eksamen/Http/QuestionnaireServer.java
+++ src/main/java/no/kristiania/eksamen/Http/QuestionnaireServer.java
@@ -10,7 +10,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-public class ProductServer {
+public class QuestionnaireServer {
 
     public static void main(String[] args) throws IOException {
         DataSource dataSource = createDataSource();
