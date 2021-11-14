aadb6420d7c6edf3b46e80e219af76ddd7862372
Eskil <eskiltb@hotmail.com>
2021-11-14 23:00:45 +0100

    Cockroach is also a bug
    And flyway version upgraded

--- src/main/java/no/kristiania/eksamen/Http/QuestionnaireServer.java
+++ src/main/java/no/kristiania/eksamen/Http/QuestionnaireServer.java
@@ -9,29 +9,26 @@ import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 import java.io.IOException;
-import java.util.Map;
 
 public class QuestionnaireServer {
-
-    private static DataSource dataSource = Datasource.createDataSource();
-    private static QuestionDao questionDao = new QuestionDao(dataSource);
-    private static AnswerDao answerDao = new AnswerDao(dataSource);
-    private static UserDao userDao = new UserDao(dataSource);
-
-    public static Map<String, HttpController> controllers = Map.of("/api/listQuestions", new ListQuestionsController(questionDao),
-            "/api/questionSelect", new QuestionSelectController(questionDao),
-            "/api/answer", new AnswerQuestionController(answerDao, questionDao),
-            "/api/newQuestion", new NewQuestionController(questionDao),
-            "/api/viewAnswers", new ViewAnswersController(answerDao),
-            "/api/alterQuestion", new AlterQuestionController(questionDao),
-            "/api/cookieAPI", new CookieCrumbController(userDao)
-    );
 
     private static final Logger logger = LoggerFactory.getLogger(HttpServer.class);
 
     public static void main(String[] args) throws IOException {
-
         HttpServer httpServer = new HttpServer(1962);
+
+        DataSource datasource = Datasource.createDataSource();
+        QuestionDao questionDao = new QuestionDao(datasource);
+        AnswerDao answerDao = new AnswerDao(datasource);
+        UserDao userDao = new UserDao(datasource);
+
+        httpServer.addController("/api/listQuestions", new ListQuestionsController(questionDao));
+        httpServer.addController("/api/questionSelect", new QuestionSelectController(questionDao));
+        httpServer.addController("/api/answer", new AnswerQuestionController(answerDao, questionDao));
+        httpServer.addController("/api/newQuestion", new NewQuestionController(questionDao));
+        httpServer.addController("/api/viewAnswers", new ViewAnswersController(answerDao));
+        httpServer.addController("/api/alterQuestion", new AlterQuestionController(questionDao));
+        httpServer.addController("/api/cookieAPI", new CookieCrumbController(userDao));
 
 
         logger.info("Starting http://localhost:{}/preIndex.html", httpServer.getPort());
